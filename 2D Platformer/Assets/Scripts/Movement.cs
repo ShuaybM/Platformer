0336089aa5e15790cd690f3ee129aa99ee89fdf4
ShuaybM <41960930+ShuaybM@users.noreply.github.com>
2021-01-12 04:16:01 +0000

    character controller
    added in brackeys player movement

---
+++ 2D Platformer/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float runSpeed = 10f;                                                        //public so it can be changed within unity itslef (small box)
+
+    float horizontalMove = 0f;                                                          //setting variables ...
+    bool jump = false;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;                     //left and right movement
+
+        if (Input.GetButtonDown("Jump"))                                                //jumping input 
+        {
+            jump = true;
+        }
+
+    }
+}
