a77cf4e73db03200377d46a27fe2b5c937ddfcbb
ShuaybM <41960930+ShuaybM@users.noreply.github.com>
2021-01-14 22:12:13 +0000

    Player movment - reset
    added the code for player movment but couldnt get it to jump. running out of time so switch back to CrossPlatformInputManager

--- 2D Platformer/Assets/Scripts/Movement.cs
+++ 2D Platformer/Assets/Scripts/Movement.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float runSpeed = 10f;                                                        //public so it can be changed within unity itslef (small box)
+    public CharacterController2D controller;                                            //referencing the character controller 2D script to allow the movement setup in the controller to be used here
 
-    float horizontalMove = 0f;                                                          //setting variables ...
-    bool jump = false;
+                                                                                       //public so it can be changed within unity itslef (small box)
+                                                                                       //setting variables ...
+    public float runSpeed = 10f;
+    float horizontalMove = 0f;                                                          //0 for no movement, -1 to move backwards (left) and 1 for forward (right)
+    bool jump = false;                                                              
 
 
 
     // Update is called once per frame
     void Update()
     {
+
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;                     //left and right movement
 
         if (Input.GetButtonDown("Jump"))                                                //jumping input 
@@ -21,5 +25,17 @@ public class Movement : MonoBehaviour
             jump = true;
         }
 
+
+        if (Input.GetButtonDown("Jump")) ;
+        {
+            jump = true;
+
+        }
+    }
+
+    private void FixedUpdate()                                                          //this will force the update to run at the same frequency that the physics system (character controlelr 2d) runs at, making it more accurate than the standard update function
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);            //using the controller, we are moving it on thge horizontal axis at the same, multiplying by the fixed time of the fixedupdate, and allowing for both jumping and crouching
+        jump = false;                                                                    //resetting the jump 
     }
 }
